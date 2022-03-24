ecdb62c464d741aa32a9af54b8551fe671f64ba3
Steve S. Lee <sjl558@nyu.edu>
2022-03-24 10:41:38 -0400

    initial commit

---
+++ src/main/java/com/example/springtestingweb/SpringTestingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtestingweb/SpringTestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
